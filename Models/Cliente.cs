ba3bb3c215dc32813eb7edb535c032b0545b2205
Williann Carlos de Freitas <williann.de.freitas@gmail.com>
2022-04-04 11:07:30 -0300

    alteração de classes Acrud

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -4,32 +4,52 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApiTeste.Models
 {
+    [Table(name: "CLIENTE")]
     public class Cliente
     {
-        [Table(name: "CLIENTE")]
-        public class Usuario
-        {
-            [Key]
-            [Column("ID_CLIENTE")]
-            public Int64 IdCliente { get; set; }
+        [Key]
+        [Column("ID_CLIENTE")]
+        public Int64 IdCliente { get; set;}
 
-            [Column("NOME")]
-            public string Nome { get; set; }
+        [Column("NOME")]
+        public string Nome { get; set; }
 
-            [Column("SOBRENOME")]
-            public string Sobrenome { get; set; }
+        [Column("SOBRENOME")]
+        public string Sobrenome { get; set; }
 
-            [Column("LOGIN")]
-            public string logIn { get; set; }
+        [Column("DDD")]
+        public string DDD { get; set; }
 
-            [Column("DT_NASCIMENTO")]
-            public DateTime Data_Nascimento { get; set; }
+        [Column("TELEFONE")]
+        public string Telefone { get; set; }
 
-            [Column("DT_INCLUSAO")]
-            public DateTime Data_Inclusao { get; set; }
+        [Column("CPF")]
+        public string Cpf { get; set; }
 
-            [Column("DT_ALTERACAO")]
-            public DateTime Data_Alteracao { get; set; }
-        }
+        [Column("RG")]
+        public string Rg { get; set; }
+
+        [Column("DT_NASCIMENTO")]
+        public DateTime Data_Nascimento { get; set; }
+
+        [ForeignKey("UsuarioInclusao")]
+        [Column("ID_USUARIO_INCLUSAO")]
+        public Int64 IdUsuarioInclusao { get; set; }
+
+        [Column("DT_INCLUSAO")]
+        public DateTime Data_Inclusao { get; set; }
+
+        [ForeignKey("UsuarioAlteracao")]
+        [Column("ID_USUARIO_ALTERACAO")]
+        public Int64 IdUsuarioAlteracao { get; set; }
+
+        [Column("DT_ALTERACAO")]
+        public DateTime Data_Alteracao { get; set; }
+
+
+
+        public virtual Usuario UsuarioInclusao { get; set; }
+
+        public virtual Usuario UsuarioAlteracao { get; set; }
     }
 }
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -11,42 +11,23 @@ namespace WebApiTeste.Models
         [Column("ID_USUARIO")]
         public Int64 IdUsuario { get; set; }
 
-        
-        [Key]
-        [Column("ID_USUARIO_INCLUSAO")]
-        public Int64 IdUsuarioInclusao { get; set; }
-
         [Column("NOME")]
         public string Nome { get; set; }
 
         [Column("SOBRENOME")]
         public string Sobrenome { get; set; }
 
-        [Column("DDD")]
-        public string DDD { get; set; }
-
-        [Column("TELEFONE")]
-        public string Telefone { get; set; }
-
-        [Column("CPF")]
-        public string Cpf { get; set; }
-
-        [Column("RG")]
-        public string Rg { get; set; }
+        [Column("LOGIN")]
+        public string logIn { get; set; }
 
         [Column("DT_NASCIMENTO")]
         public DateTime Data_Nascimento { get; set; }
 
-
-        [Key]
-        [Column("ID_USUARIO_ALTERACAO")]
-        public Int64 IdUsuarioAlteracao { get; set; }
+        [Column("DT_INCLUSAO")]
+        public DateTime Data_Inclusao { get; set; }
 
         [Column("DT_ALTERACAO")]
         public DateTime Data_Alteracao { get; set; }
-
-        [Column("DT_INCLUSAO")]
-        public DateTime Data_Inclusao { get; set; }
 
     }
 }
